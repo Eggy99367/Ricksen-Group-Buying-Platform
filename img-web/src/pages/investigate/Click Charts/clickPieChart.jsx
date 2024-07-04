8843e54da5a8cda6276419da1a0d4531c791df6a
Aaron95629 <aaron.tw629@gmail.com>
2024-07-04 23:05:18 +0800

    Update Investigate chart and fix linebot error

---
+++ img-web/src/pages/investigate/Click Charts/clickPieChart.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const ClickPieChart = ({ totalViews, totalFollowers }) => {
+  const data = {
+    labels: ['Clicked Users', 'Unclicked Users'],
+    datasets: [
+      {
+        label: '# of People',
+        data: [totalViews, totalFollowers - totalViews],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 99, 132, 0.6)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const label = context.label || '';
+            const value = context.raw || 0;
+            const total = context.dataset.data.reduce((acc, curr) => acc + curr, 0);
+            const percentage = ((value / total) * 100).toFixed(2);
+            return `${label}: ${value} (${percentage}%)`;
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div classname = "chart" style={{ height: '300px', width: '100%', marginRight: '100px' }}>
+      <Pie data={data} options={options} />
+    </div>
+  );
+};
+
+export default ClickPieChart;
