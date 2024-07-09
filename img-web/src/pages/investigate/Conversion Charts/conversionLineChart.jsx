415e319132c4d799a095ca055d6dff16a05daa54
Aaron95629 <aaron.tw629@gmail.com>
2024-07-09 15:53:15 +0800

    Update Charts Time Conversion

--- img-web/src/pages/investigate/Conversion Charts/conversionLineChart.jsx
+++ img-web/src/pages/investigate/Conversion Charts/conversionLineChart.jsx
@@ -6,16 +6,12 @@ import 'chartjs-adapter-date-fns';
 ChartJS.register(TimeScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const convertToDate = (datetimeStr) => {
-    const date = new Date(datetimeStr);
+    console.log("ok");
+    const [datePart, timePart] = datetimeStr.split('T');
+    const [year, month, day] = datePart.split('-');
+    const [hours, minutes, seconds] = timePart.split(':');
     
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-  
-    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+    return new Date(Date.UTC(year, month - 1, day, hours, minutes, seconds));
   };
 
 const ConversionLineChart = ({ timestamps, totalViewers }) => {
