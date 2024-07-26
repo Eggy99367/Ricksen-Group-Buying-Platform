c89ee902f765fddfddad05de7972cde911f10d15
Aaron95629 <aaron.tw629@gmail.com>
2024-07-26 13:30:48 +0800

    analysis page fix

--- img-web/src/pages/analysis/searchBar.jsx
+++ img-web/src/pages/analysis/searchBar.jsx
@@ -5,14 +5,11 @@ export const SearchBar = ({data, handleInputChange, selectedRow, handleInvestiga
 
     return (
         <div className='function_bar_container'>
-            <div className='empty_bar_container'></div>
+            <div className='empty_bar_container'>
+                <h2>數據分析</h2>
+            </div>
             <div className='input_entry_container'>
-                <input placeholder="搜尋團購id" autoComplete="on" list={data.attr} value={data.data} onChange={(e) => handleInputChange(e.target.value)}/> 
-                <datalist id={data.attr}>
-                    {data.options && Object.values(data.options).map((opt, index) => (
-                    <option key={index} value={opt}></option>
-                    ))}
-                </datalist>
+                <input placeholder="搜尋" autoComplete="on" list={data.attr} value={data.data} onChange={(e) => handleInputChange(e.target.value)}/> 
             </div>
             <div className='investigate button'>
                 <button disabled={selectedRow === null} onClick={() => handleInvestigateClick(selectedRow)}>查看分析</button>
